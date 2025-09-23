20ddcbee07d3ff1aa3501e0dc0f0b580b7ae399e
Tahirbasha <tahirbasha2626@gmail.com>
2025-09-23 21:53:39 +0530

    Add - Navigation to /login on invalid authorization.

--- src/body.tsx
+++ src/body.tsx
@@ -1,8 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/user-slice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err: any) {
+      console.log(err);
+      if (err.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <Navbar />
